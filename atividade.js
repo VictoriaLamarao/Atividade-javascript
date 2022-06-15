653b9b4e7da8e9c0cfedf038f09b78e3d5f354e6
Victória Lamarão <54338477+VictoriaLamarao@users.noreply.github.com>
2022-06-15 13:21:27 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,9 @@
+let nome = "Victória";
+let sobrenome = "Lamarão";
+const anoNascimento = 2000;
+const anoAtual = 2022;
+let cidade = "Porto velho";
+
+
+
+console.log(`Eu sou ${nome} ${sobrenome} tenho ${anoAtual - anoNascimento} e moro em ${cidade}`);
